4c3b66725e444adebad72d3100236ab54345d65b
Pawel Gryc <czytopawelgryc@gmail.com>
2024-03-26 21:18:34 +0100

    feat: (WIP) Task interactivity

--- frontend/src/components/Task.js
+++ frontend/src/components/Task.js
@@ -1,13 +1,39 @@
 export default function Task() {
-    return (
-        <div className="Task">
-            <div className="TaskTitle">
-                Task Title
-            </div>
-            <hr></hr>
-            <div className="TaskDescription">
-                TASK DESCRIPTION
-            </div>
-        </div>
-    )
+  function handleTitleClick() {
+    alert("clicked Title");
+  }
+
+  function handleDescriptionClick(e) {
+    let container = e.currentTarget;
+    let input = document.createElement("input");
+    input.type = "text";
+    input.value = container.firstChild.textContent;
+    container.replaceChild(input, container.firstChild);
+    input.focus();
+    input.onblur = handleDescriptionBlur;
+    input.addEventListener("keyup", function (event) {
+      if (event.key === "Enter") {
+        handleDescriptionBlur(event);
+      }
+    });
+  }
+
+  function handleDescriptionBlur(e) {
+    let container = e.currentTarget.parentNode;
+    let value = container.firstChild.value;
+    let textNode = document.createTextNode(value);
+    container.replaceChild(textNode, container.firstChild);
+  }
+
+  return (
+    <div className="Task">
+      <div className="TaskTitle" onClick={handleTitleClick}>
+        Task Title
+      </div>
+      <hr></hr>
+      <div className="TaskDescription" onClick={handleDescriptionClick}>
+        TASK DESCRIPTION
+      </div>
+    </div>
+  );
 }
